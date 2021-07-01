e6675f2f0ba5bed1eec82031498c1953d7a5d072
Mason Wallis <mwallis5110@gmail.com>
2021-07-01 09:36:02 -0500

    Daily Update

--- 04-Web-APIs/02-Homework/Solution/script.js
+++ 04-Web-APIs/02-Homework/Solution/script.js
@@ -2,24 +2,30 @@
 
 // Global Variables:
 var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var highScore = '';
 var startEl = document.getElementById('start');
 var restartEl = document.getElementById(".restart");
 var timerEl = document.getElementById('timer');  
 var secondsLeft = 120;
+var ulEl = document.getElementById('ul');
+var aa = document.getElementById('A');
+var bb = document.getElementById('B');
+var cc = document.getElementById('C');
+var dd = document.getElementById('D');
+var highScore = document.getElementById('highscore');
+var results = document.getElementById('results');
+var score = document.getElementById("score").innerHTML = 0; 
 
 // WHEN I click the start button
-    //Event Listener
+    //Event Listener to load question and start timer
 
     startEl.addEventListener('click', function (event) {
-        event.preventDefault(); 
-    }); // should load question and start timer
-
+        event.preventDefault();
+        loadQuestion();
+        setTime(secondsLeft);
+    }); 
 
 // THEN a timer starts and I am presented with a question
     //Timer
-    //setTime function - start when start button is clicked
 function setTime() { 
       // Sets interval in variable
     var timerInterval = setInterval(function() {
@@ -29,87 +35,103 @@ function setTime() {
         if (secondsLeft === 0) {
             // Stops execution of action at set interval
             clearInterval(timerInterval);
-            // Calls function to create and send message
-            sendMessage();
         };
     
     }, 1000);
 };
-
-setTime ();
-
 // WHEN I answer a question
     //Arrays for questions - One big array
-
     const questions = [
         {
             question: "Which two variables can a Boolean function return?",
             answers: ["Up/Down", "Right/Left", "True/False", "Cat/Dog"],
             correctAnswer: "C"
-            //If statement to check correctness
-        };
+        }, 
         {
-            question: "Which type of variable can be globally scoped?";
+            question: "Which type of variable can be globally scoped?",
             answers: ["const", "var", "let", "event"],
             correctAnswer: "B"
-        }
+        },
         {
-            question: "Which of the following represents abstract equality?";
+            question: "Which of the following represents abstract equality?",
             answers: ["=", "==", "$", "!=="],
             correctAnswer: "B"
-        }
+        },
         {
-            question: "Style properties can be altered with Javascript via the ______ method.";
+            question: "Style properties can be altered with Javascript via the ______ method.",
             answers: ["addEventListener", "sendMessage", "preventDefault", "setAttribute"],
-            correctAnswer: "d"
-        }
+            correctAnswer: "D"
+        },
         {
-            question: '2+"2" = __';
-            answers: ["22", "4", "7", "10"],
+            question: '2 + "2" = __',
+            answers: ["22", "4", "7", "12"],
             correctAnswer: "A"
         }
-        
     ];
-    //Function to load next question
 
+    //Function to load next question
     var currentQuestion = 0;
     function loadQuestion () {
         var data = questions [currentQuestion];
-        document.getElementById("questionSpot").textContent(data.question)
-    }
-
-
+        var Q = document.getElementById("questionSpot");
+        Q.textContent = data.question;
+        aa.textContent = data.answers[0];
+        bb.textContent = data.answers[1];
+        cc.textContent = data.answers[2];
+        dd.textContent = data.answers[3];
+    };
 // THEN I am presented with another question
     //for loop to cycle through questions
+var questionArray = questions.length;
+for (i = 0; i < questionArray; i++);
 
 // WHEN I answer a question incorrectly
 
 // THEN time is subtracted from the clock
-        //Some way to interface with the timer
+
+//Function to check correctness
 document.getElementById("answers").addEventListener("click", function (event) {
     event.preventDefault();
-    //right/wrong answer
     if (event.target.value !== questions [currentQuestion].correctAnswer) {
         secondsLeft -= 20;
-    }
-    //last question or not
-    if(currentQuestion === 4) {
-        //celebratory gif, then go to high scores page - function to go there, or when view highscores button is clicked
-            //Declare function globally, call here - Call highscores button too
+    } else {
+        score += 1;
+        console.log(score); 
+        document.getElementById("score").innerHTML = score;
+    };
+
+// WHEN all questions are answered or the timer reaches 0 
+
+// THEN the game is over
+//Checks whether it is the last question or not, or whether time is up
+
+    if (currentQuestion === 4) {
+        clearInterval(quizContainer);
+        alert("Finished!");
+        scoreName = prompt("Please enter your name for our high scores list:");
+    } else if (secondsLeft === 0) {
+        clearInterval(quizContainer);
+        alert("Time is up!");
+        scoreName = prompt("Please enter your name for our high scores list:");
     } else {
         currentQuestion++;
         loadQuestion();
-    }
-    //not last question
-});
-// WHEN all questions are answered or the timer reaches 0
+    };
 
-// THEN the game is over
-    // Trigger an event based off the timer running out - clearInterval
-    // Would this be done within the timer code or separately? - Both
-    //Functions to end quiz, populate next question, show high scores, etc
+    var finalScore = scoreName + ", " + score;
+    console.log(finalScore);
 
 // WHEN the game is over
 
 // THEN I can save my initials and my score
-    //Local Storage 
+    //Local Storage, render high scores on bottom of page 
+    // FIX THIS -> localStorage.setItem("finalScore", JSON.stringify(finalScore)); 
+});
+
+// // //use .append to add names and score to HTML table
+
+// var highestScores = function () {
+//     if (finalScore < document.getElementById("3rd"));
+//         finalScore.appendChild("3rd");
+//     } else if (highScore > document.getElementById("3rdScore"));
+//  )};
---
+++ Untitled-2.js
@@ -0,0 +1,17 @@
+// Pseudocode 
+// 1. User inputs info into a form
+    document.getElementById('myForm').onsubmit();
+        event.preventDefault
+//     event.preventDefault
+// 2. User clicks button
+// When button is clicked (event listener on button):
+
+// 1. Grab form values, store as variable
+//     var email =
+//     var password =
+// 2. Use variable to setItem in local storage
+// 3. Use getItem to read from local storage
+// 4. Might need to parse before appending to page
+// 5. Set HTML element as var
+
+// 6. Set html element give class, data-attr, textContent, innerHTML
